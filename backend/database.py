6dbdc1e5fd9458fbe1ff9ec2707997ba9e8413c0
SAMALA SANJAY <67814280+sanjaysam410@users.noreply.github.com>
2025-10-31 13:15:12 +0530

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+# backend/database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://apple@localhost/apple"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
